0ec8a53c9ebe91d7a2740e0d8750797df13240fa
alejandraarche <alejandra.arche@opendeusto.es>
2022-05-24 17:38:38 +0200

    Table

--- src/main/java/supermarket/client/VentanaPerfil.java
+++ src/main/java/supermarket/client/VentanaPerfil.java
@@ -6,138 +6,146 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 
-import supermarket.db.Db;
+import supermarket.domain.Order;
 import supermarket.domain.User;
+
 
 public class VentanaPerfil extends JFrame {
 
 
 	private JTable jTable;
-	private String[][] tableData =
-			{{"01", "Adam", "1986"},
-			{"02", "John", "1990"},
-			{"03", "Sam", "1989"},
-			{"04", "Derek", "1991"},
-			{"05", "Ben", "1981"},
-			{"06", "Alice", "1981"},
-			{"07", "Harry", "1981"},
-			{"08", "Bob", "1981"}};
+	private String[][] tableData;
 	private String[] tableColumn = {"Order ID", "Date", "Amount spended"};
+	private DefaultTableModel model;
+	JTable tabla = new JTable(model);
+
+
 	//private JScrollPane jScrollPane;
 
 
-	private JPanel contentPane, jpCenter, jpCenterLeft, jpCenterRight, jpSouth, jpButton;
+	private JPanel contentPane, jpCenter, jpCenterLeft, jpCenterRight, jpSouth, jpCenterUp, jpCenterDown, jpButton;
 	private JTable jtOrderRecord;
 	private JLabel jlPhoto, jlName, jlSurName, jlID, jlEmail, jlNick, jlUserName, jlUserSurName, jlUserID, jlUserEmail, jlTable;
 	private JButton jbReturn;
+	private JScrollPane jScrollPane;
 
 
 
 
 	public VentanaPerfil(JFrame ventanaAnterior, User user) {
-			//Characteristics of the main window
-			/*Db db = new Db();
-			db.connect();
+		//Characteristics of the main window
+		/*Db db = new Db();
+		db.connect();
+		user=db.getUser(user.getUsername());
+		db.disconnect();*/
+		setTitle("Profile");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 700, 900);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
 
-			user=db.getUser(user.getUsername());
+		//Panels
 
-			db.disconnect();*/
+		jpCenter = new JPanel();
+		contentPane.add(jpCenter, BorderLayout.CENTER);
+		jpCenter.setLayout(new GridLayout(2, 1, 0, 0));
 
-			setTitle("Profile");
+		jpCenterUp = new JPanel();
+		jpCenter.add(jpCenterUp);
+		jpCenterUp.setLayout(new GridLayout(1, 2, 0, 0));
 
-			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			setBounds(100, 100, 700, 900);
-			contentPane = new JPanel();
-			contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-			contentPane.setLayout(new BorderLayout(0, 0));
-			setContentPane(contentPane);
+		jpCenterDown = new JPanel();
+		jpCenter.add(jpCenterDown);
+		jpCenterDown.setLayout(new GridLayout(2, 1, 0, 0));
 
-			//Panels
+		jpCenterLeft = new JPanel();
+		jpCenterUp.add(jpCenterLeft);
+		jpCenterLeft.setLayout(new GridLayout(5, 1, 0, 0));
 
-			jpCenter = new JPanel();
-			contentPane.add(jpCenter, BorderLayout.CENTER);
-			jpCenter.setLayout(new GridLayout(1, 2, 0, 0));
+		jpCenterRight = new JPanel();
+		jpCenterUp.add(jpCenterRight);
+		jpCenterRight.setLayout(new GridLayout(5, 1, 0, 0));
 
-			jpCenterLeft = new JPanel();
-			jpCenter.add(jpCenterLeft);
-			jpCenterLeft.setLayout(new GridLayout(5, 1, 0, 0));
+		jpSouth = new JPanel();
+		contentPane.add(jpSouth, BorderLayout.SOUTH);
+		jpSouth.setLayout(new GridLayout(1, 0, 0, 0));
 
-			jpSouth = new JPanel();
-			contentPane.add(jpSouth, BorderLayout.SOUTH);
-			jpSouth.setLayout(new GridLayout(3, 0, 0, 0));
+		//Labels
 
+		jlPhoto = new JLabel();
+		jlPhoto.setIcon(new ImageIcon("src/main/java/supermarket/client/images/userFoto.png"));
+		jlPhoto.setHorizontalAlignment(SwingConstants.CENTER);
+		jpCenterLeft.add(jlPhoto);
 
-			jpCenterRight = new JPanel();
-			jpCenter.add(jpCenterRight);
-			jpCenterRight.setLayout(new GridLayout(5, 0, 0, 0));
+		jlName = new JLabel("Name");
+		jlName.setHorizontalAlignment(SwingConstants.RIGHT);
+		jpCenterLeft.add(jlName);
 
-			//Labels
+		jlSurName = new JLabel("Surname");
+		jlSurName.setHorizontalAlignment(SwingConstants.RIGHT);
+		jpCenterLeft.add(jlSurName);
 
-			jlPhoto = new JLabel();
-			jlPhoto.setIcon(new ImageIcon("src/main/java/supermarket/client/images/userFoto.png"));
-			jlPhoto.setHorizontalAlignment(SwingConstants.CENTER);
-			jpCenterLeft.add(jlPhoto);
+		jlID = new JLabel("ID");
+		jlID.setHorizontalAlignment(SwingConstants.RIGHT);
+		jpCenterLeft.add(jlID);
 
-			jlName = new JLabel("Name");
-			jlName.setHorizontalAlignment(SwingConstants.RIGHT);
-			jpCenterLeft.add(jlName);
+		jlEmail = new JLabel("E-mail");
+		jlEmail.setHorizontalAlignment(SwingConstants.RIGHT);
+		jpCenterLeft.add(jlEmail);
 
-			jlSurName = new JLabel("Surname");
-			jlSurName.setHorizontalAlignment(SwingConstants.RIGHT);
-			jpCenterLeft.add(jlSurName);
+		jlNick = new JLabel("@" + user.getUsername());
+		jpCenterRight.add(jlNick);
 
-			jlID = new JLabel("ID");
-			jlID.setHorizontalAlignment(SwingConstants.RIGHT);
-			jpCenterLeft.add(jlID);
+		jlUserName = new JLabel("   " + user.getName());
+		jpCenterRight.add(jlUserName);
 
-			jlEmail = new JLabel("E-mail");
-			jlEmail.setHorizontalAlignment(SwingConstants.RIGHT);
-			jpCenterLeft.add(jlEmail);
+		jlUserSurName = new JLabel("   " + user.getLastName());
+		jpCenterRight.add(jlUserSurName);
 
-			jlNick = new JLabel("@" + user.getUsername());
-			jpCenterRight.add(jlNick);
+		jlUserID = new JLabel(String.valueOf("   " + user.getId()));
+		jpCenterRight.add(jlUserID);
 
-			jlUserName = new JLabel("   " + user.getName());
-			jpCenterRight.add(jlUserName);
+		jlUserEmail = new JLabel("   " + user.getEmail());
+		jpCenterRight.add(jlUserEmail);
 
-			jlUserSurName = new JLabel("   " + user.getLastName());
-			jpCenterRight.add(jlUserSurName);
-
-			jlUserID = new JLabel(String.valueOf("   " + user.getId()));
-			jpCenterRight.add(jlUserID);
-
-			jlUserEmail = new JLabel("   " + user.getEmail());
-			jpCenterRight.add(jlUserEmail);
-
-			jlTable = new JLabel("Client order records");
-			jlTable.setHorizontalAlignment(SwingConstants.CENTER);
-			jpSouth.add(jlTable);
+		jlTable = new JLabel("Client order records");
+		jlTable.setHorizontalAlignment(SwingConstants.CENTER);
+		jpCenterDown.add(jlTable);
 
 			//Table
+		model = new DefaultTableModel(tableData, tableColumn);
+		jtOrderRecord = new JTable(model);
+		jtOrderRecord.setShowGrid(true);
+		jtOrderRecord.setShowVerticalLines(true);
+		jScrollPane = new JScrollPane(jtOrderRecord);
+		jpCenterDown.add(jScrollPane);
 
-			jtOrderRecord = new JTable(tableData, tableColumn);
-			//jScrollPane= new JScrollPane(jtOrderRecord);
-			jpSouth.add(jtOrderRecord);
+		for(Order o : user.getOrderList()){
+			model.addRow(new Object[]{o.getId(), o.getDate(), o.getPrice()});
+		}
 
-			//Buttons
+		//Buttons
 
-			jpButton = new JPanel();
-			jpSouth.add(jpButton);
+		jpButton = new JPanel();
+		jpSouth.add(jpButton);
 
-			jbReturn = new JButton("Go back");
-			jpButton.add(jbReturn);
+		jbReturn = new JButton("Go back");
+		jpButton.add(jbReturn);
 
 
 			//ActionListeners
 
 
-			setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-			setResizable(false);
-			setSize(1150, 505);
+			//setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			//setResizable(false);
+			//setSize(1150, 505);
 
-			setLocationRelativeTo(null);
-			getContentPane().setLayout(new BorderLayout(0, 0));
+			//setLocationRelativeTo(null);
+			//getContentPane().setLayout(new BorderLayout(0, 0));
 
 
 			jbReturn.addActionListener(new ActionListener() {
@@ -149,7 +157,11 @@ public class VentanaPerfil extends JFrame {
 			});
 
 			setVisible(true);
+
+
 		}
+
+
 
 
 	}
